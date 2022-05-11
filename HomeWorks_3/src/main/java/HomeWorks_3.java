829c1e4b84bb08dae7fa8b738bf0d37b8f5b67f0
Vadim <bondarik15@gmail.com>
2022-05-12 00:27:35 +0300

    HomeWorks_3 Что успел сделать. Остальные здания сделаю

---
+++ HomeWorks_3/src/main/java/HomeWorks_3.java
@@ -0,0 +1,57 @@
+import java.util.Scanner;
+
+
+public class HomeWorks_3 {
+    public static void main(String[] args) {
+        //englishLitters();
+        //parityСheck();
+        modulNambers();
+    }
+
+    public static void englishLitters() {
+        char litters;
+        for (int i = 97; i < 123; i++) {
+            litters = (char) i;
+            System.out.println(litters);
+        }
+        for (int i = 65; i < 91; i++) {
+            litters = (char) i;
+            System.out.println(litters);
+        }
+    }
+
+    public static void parityСheck() {
+        Scanner console = new Scanner(System.in);
+        String str = console.nextLine();
+        if ((str.contains(".")) || (str.contains(","))) {
+            System.out.println("VVedeno nevernoe chislo");
+
+        } else {
+
+            int namber = Integer.parseInt(str);
+            if (namber % 2 == 0) {
+                System.out.println("Chislo chetnoe");
+            } else {
+                System.out.println("Chislo ne chetnoe");
+            }
+        }
+
+    }
+
+    public static void modulNambers() {
+        Scanner console = new Scanner(System.in);
+        double[] arrayDouble = new double[3];
+        for (int i = 0; i < 3; i++) {
+            arrayDouble[i] = console.nextDouble();
+        }
+        double modulMin = arrayDouble[0];
+        for (int i = 1; i < 3; i++) {
+            if (Math.abs(modulMin) > Math.abs(arrayDouble[i])) {
+                modulMin = arrayDouble[i];
+            }
+        }
+        System.out.println(modulMin);
+    }
+
+
+}
---
+++ Lesson3/src/main/java/Lesson3.java
@@ -0,0 +1,28 @@
+
+public class Lesson3 {
+    public static void main(String[] args) {
+
+        boolean b = true;
+        boolean result = !b ? 3 > 2 : 5 < 7;
+        /*
+        if (!b){
+            result = 3>2;
+        } esle {
+            result = 5>7;
+        }
+            */
+        int mm = 8;
+        mm += 2;
+        System.out.println("привет");
+        Random random = new Random();
+        int[][] array = new int[10][10];
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                array = random.;
+            }
+            System.out.println();
+
+        }
+    }
+}
+
