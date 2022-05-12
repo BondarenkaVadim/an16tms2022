336796155180d9dc7c792bb563c6e11de6d4cbce
Vadim <bondarik15@gmail.com>
2022-05-12 20:50:48 +0300

    Lesson4

--- Lesson3/src/main/java/Lesson3.java
+++ Lesson3/src/main/java/Lesson3.java
@@ -2,8 +2,8 @@
 public class Lesson3 {
     public static void main(String[] args) {
 
-        boolean b = true;
-        boolean result = !b ? 3 > 2 : 5 < 7;
+        //boolean b = true;
+        // boolean result = !b ? 3 > 2 : 5 < 7;
         /*
         if (!b){
             result = 3>2;
@@ -11,7 +11,7 @@ public class Lesson3 {
             result = 5>7;
         }
             */
-        int mm = 8;
+      /*  int mm = 8;
         mm += 2;
         System.out.println("привет");
         Random random = new Random();
@@ -20,9 +20,31 @@ public class Lesson3 {
             for (int j = 0; j < array[i].length; j++) {
                 array = random.;
             }
-            System.out.println();
+            System.out.println();*/
 
+        int[] ints = new int[10];
+        Random random = new Random();
+        for (int j = 0; j < ints.length; j++) {
+            ints[j] = random.nextInt(11); // от 0 до 10
         }
+        //    ints = random.nextInt(11) + 10; // от 10 до 20
+        //    ints = random.nextInt(21) - 10; // от -10 до 10
+        int[] intsNew = new int[15];
+
+        System.arraycopy(ints, 0, intsNew, 0, ints.length);//копировать массив
+        System.out.println(Arrays.toString(intsNew)); // ввывод всего массива
+
+
+        // Arrays.equals(temp, temp2) // сравнение массива
+        Arrays.sort(intsNew); // сортировка массива (по возрастанию)
+        System.out.println(Arrays.toString(intsNew));
+        /*
+        swinc
+        case
+
+        тернарная перация
+         */
     }
 }
+
 
---
+++ Lesson4/src/main/java/Lesson4.java
@@ -0,0 +1,62 @@
+import java.util.Arrays;
+import java.util.Random;
+
+public class Lesson4 {
+    public static void main(String[] args) {
+
+        int[] ints = new int[10];
+        Random random = new Random();
+        for (int j = 0; j < ints.length; j++) {
+            ints[j] = random.nextInt(11); // от 0 до 10
+        }
+        //    ints = random.nextInt(11) + 10; // от 10 до 20
+        //    ints = random.nextInt(21) - 10; // от -10 до 10
+        int[] intsNew = new int[15];
+
+        System.arraycopy(ints, 0, intsNew, 0, ints.length);//копировать массив
+        System.out.println(Arrays.toString(intsNew)); // ввывод всего массива
+
+
+        // Arrays.equals(temp, temp2) // сравнение массива
+        Arrays.sort(intsNew); // сортировка массива (по возрастанию)
+        System.out.println(Arrays.toString(intsNew));
+        //_____________________________-
+        /*
+        switch ()
+        case
+
+        default
+
+        тернарная операция
+         */
+
+        int[] mass = new int[3000];
+        boolean b = true;
+        for (int i = 0; i < mass.length && b; i += 3) {
+            System.out.println("1");
+            if (i == 3) {
+                b = false;
+            }
+        }// 1 напечатает 2 раза
+        /* бесконечный цикл
+        for (; ; ){
+         */
+        /*
+        }*/
+        /*for (int i = 0; int j = 10;  i <j ; i++; j--) {
+            System.out.println(i + " " + j);
+        }
+        */
+        System.out.println("___________");
+        kr();
+
+    }
+
+    public static void kr() {
+        for (int i = 0; i <= 100; i += 5) //{
+            // if (i % 5 == 0) {
+            System.out.print(i + " ");
+        //  } вывести числа кратные 5 оптимальной итерации
+
+    }
+}
