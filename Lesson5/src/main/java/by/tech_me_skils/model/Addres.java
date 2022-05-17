98c74996f890e244a04f63f1821269b3ddc34c21
Vadim <bondarik15@gmail.com>
2022-05-17 22:00:44 +0300

    Lesson5 with test Class

---
+++ Lesson5/src/main/java/by/tech_me_skils/model/Addres.java
@@ -0,0 +1,16 @@
+package by.tech_me_skils.model;
+
+public class Addres {
+    public String country;
+    public String city;
+    public String street;
+
+    @java.lang.Override
+    public java.lang.String toString() {
+        return "Addres{" +
+                "country=" + country +
+                ", city=" + city +
+                ", street=" + street +
+                '}';
+    }
+}
