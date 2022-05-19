305fa31c1a44cc51df2394b4430decbe4f0bd091
Vadim <bondarik15@gmail.com>
2022-05-19 11:41:43 +0300

    HW5 Исправил

--- HomeWorsk5/src/main/java/by/tms/hw5/person/main/Main.java
+++ HomeWorsk5/src/main/java/by/tms/hw5/person/main/Main.java
@@ -1,4 +1,20 @@
 package by.tms.hw5.person.main;
 
 public class Main {
+    public static void main(String[] args) {
+
+        Person person = new Person();
+        person.name = "Nikolay";
+        person.age = 12;
+        person.heigth = 150;
+
+
+        Addres addres = new Addres();
+        addres.city = "Minsk";
+        addres.country = "Belarus";
+        addres.street = "Mira";
+
+        person.addres = addres; // идет в person и из person в addres
+        System.out.println(person);
+    }
 }
