a638862da81e0e7c54d8e05b2f2e8f5d90e51c3f
Vadim <bondarik15@gmail.com>
2022-05-19 16:38:54 +0300

    HW5 Не до концо понял какую именно логику нужно было сделать, сделал вот такую

--- HomeWorsk5/src/main/java/by/tms/hw5/person/main/Main.java
+++ HomeWorsk5/src/main/java/by/tms/hw5/person/main/Main.java
@@ -3,18 +3,30 @@ package by.tms.hw5.person.main;
 public class Main {
     public static void main(String[] args) {
 
-        Person person = new Person();
-        person.name = "Nikolay";
-        person.age = 12;
-        person.heigth = 150;
+        User testUser = new User(55);  // если 55 лет то пожилой мужчина
+        testUser.firstName = "IvanovOld";
+        testUser.lastName = "IvanOld";
+        testUser.sex = "Men";
+
+        User testUser2 = new User("Girl");
+
+        User user = new User();
+        user.firstName = "Bondarenko";
+        user.lastName = "Dima";
+        user.age = 33;
+        user.sex = "Men";
 
 
-        Addres addres = new Addres();
-        addres.city = "Minsk";
-        addres.country = "Belarus";
-        addres.street = "Mira";
+        user.ageIncrease();
+        user.fullLastName();
+        System.out.println(testUser2);
+        System.out.println("_____________-");
+        System.out.println(testUser);
+        System.out.println("______");
+        System.out.println(user);
 
-        person.addres = addres; // идет в person и из person в addres
-        System.out.println(person);
+        System.out.println("_____________________");
+
+
     }
 }
