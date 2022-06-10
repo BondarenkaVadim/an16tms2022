33a91d87c7d6bc797803ee776f037e4b51b6ea8d
Vadim <bondarik15@gmail.com>
2022-06-10 14:27:16 +0300

    HW11

--- HomeWork11/src/main/java/by/tms/hw11/workingWithRegularExpressions/Main.java
+++ HomeWork11/src/main/java/by/tms/hw11/workingWithRegularExpressions/Main.java
@@ -32,18 +32,9 @@ public class Main {
         }
 
         //4 заданние
-//        {
-//            String str = "One two three раз два три one1 two2 123 One12One One";
-//            System.out.println(str.split("\\s*([A-Za-z]+)\\s+|\\s+([A-Za-z]+)$").length);// почему считает не верное? выражение то вреде верное
-//            System.out.println(Arrays.toString(str.split("\s* \\w+")));
-//
-//            //Pattern compile = Pattern.compile("[A-Za-z]*[A-Za-z]\\ |\s[A-Za-z]*[A-Za-z]$");
-//            Pattern compile = Pattern.compile("\\s*([A-Za-z]+)\\s+|\\s+([A-Za-z]+)$");
-//            // compile = Pattern.compile("\\s*([A-Za-z]+)\\s+");
-//            Matcher matcher = compile.matcher(str);
-//            while (matcher.find()) {
-//                System.out.println(matcher.group());
-//            }
-//        }
+        {
+            String str = "One two three раз два три one1 two2 123 123";
+            System.out.println(str.split("[ А-Яа-я0-9]+|([A-Za-z]+[0-9]+)").length);
+        }
     }
 }
